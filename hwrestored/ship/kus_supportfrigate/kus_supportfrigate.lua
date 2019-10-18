ff8059f653e839bf62b916340628b7d1dea351b0
Stuart Hepworth <robostuart@gmail.com>
2019-10-18 14:50:27 -0600

    Update supportfrig scripts.

--- hwrestored/ship/kus_supportfrigate/kus_supportfrigate.lua
+++ hwrestored/ship/kus_supportfrigate/kus_supportfrigate.lua
@@ -4,7 +4,7 @@ end
 
 function Create_Kus_SupLatchDock(CustomGroup, playerIndex, shipID)
 	SobGroup_Create("LatchDockTempGroup"..shipID)
-	SobGroup_Create("LatchDockTempGroup")
+	SobGroup_CreateIfNotExist("LatchDockTempGroup")
 end
 
 function Update_Kus_SupLatchDock(CustomGroup, playerIndex, shipID)
@@ -13,27 +13,26 @@ function Update_Kus_SupLatchDock(CustomGroup, playerIndex, shipID)
 			if (SobGroup_IsDoingAbility("LatchDockTempGroup"..shipID, AB_Dock)==0) then
 				SobGroup_DockSobGroupAndStayDocked("LatchDockTempGroup"..shipID, CustomGroup)
 			end
-			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup")
-			if (SobGroup_Count("LatchDockTempGroup")==SobGroup_Count("LatchDockTempGroup"..shipID)) then --All ships docked
-				SobGroup_SetHidden( "LatchDockTempGroup"..shipID, 0)
-				SobGroup_MakeSelectable("LatchDockTempGroup"..shipID,1)
-				SobGroup_Clear("LatchDockTempGroup"..shipID)
-			end
 			SobGroup_Clear("LatchDockTempGroup")
-		elseif(SobGroup_GetROE(CustomGroup)==0)then --At Aggressive ROE, launch all ships
-			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup"..shipID)
-			SobGroup_Launch("LatchDockTempGroup"..shipID, CustomGroup)
-			SobGroup_Clear("LatchDockTempGroup"..shipID)
-		elseif(SobGroup_GetROE(CustomGroup)==2)then --At Passive ROE, all ships stay docked
-			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup"..shipID)
-			SobGroup_ForceStayDockedIfDocking("LatchDockTempGroup"..shipID)
-			SobGroup_Clear("LatchDockTempGroup"..shipID)
+			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup")
+			if (SobGroup_Count("LatchDockTempGroup")>=SobGroup_Count("LatchDockTempGroup"..shipID)) then --All ships docked
+				Destroy_LatchDock(CustomGroup, playerIndex, shipID)
+			end
+		elseif(SobGroup_GetStance(CustomGroup)==0)then --At offensive Stance, launch all ships
+			SobGroup_Clear("LatchDockTempGroup")
+			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup")
+			SobGroup_Launch("LatchDockTempGroup", CustomGroup)
+		elseif(SobGroup_GetStance(CustomGroup)==1)then --At evasive ROE, all ships stay docked
+			SobGroup_Clear("LatchDockTempGroup")
+			SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup")
+			SobGroup_ForceStayDockedIfDocking("LatchDockTempGroup")
 		end
 	elseif (SobGroup_Empty("LatchDockTempGroup"..shipID)==1) then --Hyperspacing, get all ships docked with
 		SobGroup_GetSobGroupDockedWithGroup(CustomGroup, "LatchDockTempGroup"..shipID)
 		SobGroup_AbilityActivate(CustomGroup, AB_Lights, 0)
 		SobGroup_SetHidden( "LatchDockTempGroup"..shipID, 1)
 		SobGroup_MakeSelectable("LatchDockTempGroup"..shipID,0)
+		SobGroup_AbilityActivate("LatchDockTempGroup"..shipID, AB_Attack, 0)
 	else --Hyperspacing with ships docked
 		if (SobGroup_AreAllInHyperspace("LatchDockTempGroup"..shipID)==0)and(SobGroup_CanDoAbility(CustomGroup,AB_Lights)==0) then --Entering Hyperspace
 			SobGroup_Despawn("LatchDockTempGroup"..shipID)
@@ -51,6 +50,7 @@ function Destroy_Kus_SupLatchDock(CustomGroup, playerIndex, shipID)
 	if (SobGroup_Empty("LatchDockTempGroup"..shipID)==0) then
 		SobGroup_MakeSelectable("LatchDockTempGroup"..shipID,1)
 		SobGroup_SetHidden( "LatchDockTempGroup"..shipID, 0)
+		SobGroup_AbilityActivate("LatchDockTempGroup"..shipID, AB_Attack, 1)
 		SobGroup_Clear("LatchDockTempGroup"..shipID)
 	end
 end
